204b87b4e12105e3c280e51ed39c3dd97b6e7d61
MSarfarazH <m.s.hussain@outlook.com>
2020-11-19 18:58:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ) {
+    const people = [ 'Lisa', 'Kaitlin', 'Jan' ] 
+   let writeCards = []
+    for (let i = 0; i < people.length; i++){
+        writeCards.push( `Thank you, ${people[i]}, for the wonderful ${event} gift!` )
+    }
+    return writeCards
+}
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+countDown(10);
